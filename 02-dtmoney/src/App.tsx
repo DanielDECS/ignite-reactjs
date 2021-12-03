1c507bc9993f2b679ba5f3b2a7e15045f94e7ff6
Daniel <dancarsoa@gmail.com>
2021-12-03 12:28:22 -0300

    Incluido o projeto 02-dtmoney

---
+++ 02-dtmoney/src/App.tsx
@@ -0,0 +1,12 @@
+
+import { GlobalStyle } from './global';
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
