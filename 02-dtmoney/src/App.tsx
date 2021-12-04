b49ded3bf1c56b16f4b075d7d20d33909370d5bc
Daniel <dancarsoa@gmail.com>
2021-12-03 22:41:42 -0300

    Criado os componentes da aplicação

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,12 +1,16 @@
 
+import { Dashboard } from './components/Dashboad';
+import { Header } from './components/Header';
 import { GlobalStyle } from './global';
+
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
